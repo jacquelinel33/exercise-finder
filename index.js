17c9a577a08a875bc3c9715f44d29c090ff2b278
Jacqueline Lee <jlee4332@gmail.com>
2024-02-06 13:24:27 -0800

    add logic to display all exercise according to muscle type

--- index.js
+++ index.js
@@ -4,21 +4,30 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const headers = {
+  headers: {
+    "X-Api-Key": "7Edg+szihBrax1upKTl0sw==kqDb8HhSkVobSg1P",
+  },
+};
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
 
-app.post('/submit', (req,res) => {
-    console.log(req.body)
-    res.render("index.ejs", {data: JSON.stringify(req.body)})
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
+app.post("/submit", async (req, res) => {
+
+  let response = await axios.get(
+    `https://api.api-ninjas.com/v1/exercises?muscle=${req.body.muscle}`,
+    headers
+  );
+
+  res.render("index.ejs", { data: response.data });
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
-  
+  console.log(`Server running on port: ${port}`);
+});
